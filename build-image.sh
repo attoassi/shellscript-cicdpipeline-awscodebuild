9fcbc9861ba74bb5506fb953a062849848ff4e5f
attoassi <attot.assi@gmail.com>
2023-05-31 00:49:17 -0400

    new update

--- build-image.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# fail on any error
-set -eu
-
-# build the docker image
-
-# add environment variable to the docker build command that will be used by code build to build multiple images
-docker build -f $IMAGE_TAG/Dockerfile -t $IMAGE_TAG .
